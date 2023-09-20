ddb80ea5b4c243ec5bfa834df5bff456d87c6adb
anupamabarua <anupamabaruaa@gmail.com>
2023-09-19 21:48:54 -0400

    most of this dosent makes sense

---
+++ gasplessons/sheet3/challange5.py
@@ -0,0 +1,13 @@
+from gasp import *
+
+begin_graphics()
+
+Circle((300, 200), 100)
+Circle((230,220),20)
+Circle((360,220),20)
+Line((290,190),(290,210))
+Arc((300, 200), 50, 225, 315)
+
+update_when('key_pressed') 
+
+end_graphics()
