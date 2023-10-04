0a79858501f169fb5434d99e55ccaadf158e1629
anupamabarua <anupamabaruaa@gmail.com>
2023-10-04 10:52:13 -0400

    now i finshed it

--- gasplessons/sheet5/challange1.py
+++ gasplessons/sheet5/challange1.py
@@ -1,17 +1,14 @@
-from gasp import *       
-from random import randint
+from gasp import *
 
 def place_player():
-    print("Here I am!")
-    player_x=randint(5,630)
-    player_y=randint(5,470)
-    c=Circle((player_x, player_y), 5, filled=True)
+    print("here I am")
 
 def move_player():
-    print("I'm moving...")
+    print("im moving...")
     update_when('key_pressed')
 
-begin_graphics()         
+
+begin_graphics()
 finished = False
 
 place_player()
@@ -19,4 +16,5 @@ place_player()
 while not finished:
     move_player()
 
-end_graphics()              
+
+end_graphics
