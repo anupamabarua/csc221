adbb525a96561100d08d068586fc6ef147251a01
anupamabarua <anupamabaruaa@gmail.com>
2023-10-04 11:00:58 -0400

    i dont understand robots

--- gasplessons/sheet5/challange2.py
+++ gasplessons/sheet5/challange2.py
@@ -1,14 +1,17 @@
-from gasp import *
+from gasp import *         
+from random import randint
 
 def place_player():
-    print("Here I am")
+    print("Here I am!")
+    player_x=randint(5,630)
+    player_y=randint(5,470)
+    c=Circle((player_x, player_y), 5, filled=True)
 
 def move_player():
-    print("Im moving...")
+    print("I'm moving...")
     update_when('key_pressed')
 
-
-begin_graphics()
+begin_graphics()           
 finished = False
 
 place_player()
@@ -16,5 +19,4 @@ place_player()
 while not finished:
     move_player()
 
-
-end_graphics
+end_graphics()  
