38817491ddcfa3a3fe38781c1f9d7ab9b05ed741
anupamabarua <anupamabaruaa@gmail.com>
2023-10-26 23:01:30 -0400

    chapter 5

--- chapterexcersize/chapter5/ch5ex3.py
+++ chapterexcersize/chapter5/ch5ex3.py
@@ -1,23 +1,19 @@
-def even(nums):
-    for n in nums:
-        if n // 2 == 0:
-            odd += n
-        else:
-            continue
+def keep_only_evens(nums):
+    inew=[]
+    for each in nums:
+        if each%2==0:
+            new.append(each)
+    print(new)
 
     """
-      
       >>> only_evens([3, 8, 5, 4, 12, 7, 2])
       [8, 4, 12, 2]
-      
       >>> my_nums = [4, 7, 19, 22, 42]
       >>> only_evens(my_nums)
       [4, 22, 42]
-      
       >>> my_nums
       [4, 7, 19, 22, 42]
-
-"""
+    """
 
 if __name__ == '__main__':
     import doctest
